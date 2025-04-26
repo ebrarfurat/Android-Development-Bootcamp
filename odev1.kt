5a020cd3661c7167d32cc9f17c18bdce0561e6da
ebrarfurat <153426653+ebrarfurat@users.noreply.github.com>
2025-04-26 15:18:26 +0300

    Add files via upload

---
+++ odev1.kt
@@ -0,0 +1,52 @@
+
+fun main(){
+    val sehir =  "İstanbul"
+    val ulke =  "Turkiye"
+    val telefon = 5469286172
+    val postakodu = 34748
+    val email = "deneme@gmail.com"
+    val meslek = "muhendis"
+    val stokmiktari = 15
+    val musteriadi = "selin"
+    val bakiye = 6830
+    val dogumgunu = 040804
+    val maas = 100000
+    val medenidurum = "bekar"
+    val urunYorum = "begendim"
+
+    val odemeTarihi = "2025-05-01"
+    val odeme = 499.99
+    val siparisAdeti = 8
+    val arabaModeli = "Mini"
+    val kitapAdi = "Kotlin ile Programlama"
+    val yayinlamaTarihi = "2022-12-12"
+    val indirimMiktari = 50
+    val odaSayisi = 9
+    val enlem = 47.542
+    val boylam = 10.6284
+    val urunAdi = "Klavye"
+    val yemekFiyati = 100.0
+
+    val marka = "Apple"
+    val muzikAdi = "Italy Forma"
+    val videoSuresi = "11:01"
+    val urunPuani = 5.0
+    val resimAdi = "cv.jpg"
+    val dosyaFormati = "jpg"
+    val renk = "Pembe"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "iPhone 13"
+    val ekranBoyutu = 8.1
+    val agirlik = 180.5
+
+    val ulusalGun = "23 Nisan"
+    val tatilGunu = "2025-04-03"
+    val rezervasyonTarihi = "2025-06-15"
+    val sokakAdi = "Atatürk Caddesi"
+    val otobusHatti = "34A"
+    val kalanDakika = 15
+    val takipKodu = "TR123456789"
+    val kuponSuresi = "30 gün"
+    val kuponKodu = "INDIRIM30"
+    val faturaAdresi = "Atasehir, İstanbul, Türkiye"
+}
